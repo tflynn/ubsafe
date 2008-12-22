eacfaea17b647c792837948b2349d1d7f203ae3b
Tracy Flynn <tflynn@helium.com>
2008-12-22 17:26:20 -0500

    Beginnings of configuration system

--- lib/ubsafe/ubsafe_config.rb
+++ lib/ubsafe/ubsafe_config.rb
@@ -1,4 +1,5 @@
 require 'optparse'
+require 'yaml'
 
 module UBSafe
   
@@ -9,32 +10,43 @@ module UBSafe
     class << self
 
       ##
-      # Get configuration values
+      # Get configuration settings
       #
-      # @param [Array] args Command-line arguments
       # @return [UBSafe::Config] Singleton instance of UBSafe::Config
       #
-      def config(args = nil)
-        @@config_instance = UBSafe::Config.new(args) unless defined?(@@config_instance)
+      def config
+        @@config_instance = UBSafe::Config.new unless defined?(@@config_instance)
         return @@config_instance
       end
       
     end
+
+    ##
+    # Load a Config instance
+    #
+    # @param [Array] args Command-line arguments
+    #
+    def load(args = nil)
+      @options[:config_file] = ENV['UBSAFE_CONFIG_FILE'] unless ENV['UBSAFE_CONFIG_FILE'].nil?
+      # puts "UBConfig.load After env check @options #{@options.inspect}"
+      @options.merge!(parse_args(args))
+      # puts "UBConfig.load After merge @options #{@options.inspect}"
+      if @options[:config_file]
+        @options.merge!(load_config_file(@options[:config_file]))
+      end
+    end
+
+    private
     
     ##
     # Create a new Config instance
     #
-    # @param [Array] args Command-line arguments
     # @return [UBSafe::Config] Instance of UBSafe::Config
     #
-    def initialize(args = nil)
-      @config_values = {}
-      @initial_args = args
-      @options = parse_args(args)
+    def initialize
+      @options = {}
     end
-    
-    private
-    
+
     ##
     # Parse command-line arguments
     #
@@ -42,15 +54,34 @@ module UBSafe
     # @return [Hash] Parsed options. :remainder contains any options not part
     #
     def parse_args(args = nil)
-      parsed_options = {}
+      parsed_options = {:remainder => []}
       if args and args.kind_of?(Array)
         options = OptionParser.new
-        options.on("-c","--config CONFIG_FILE", String) {|val| parsed_options[:config_file] = val}
+        options.on("-c","--config CONFIG_FILE", String, "Name of configuration file") {|val| parsed_options[:config_file] = val}
         parsed_options[:remainder] = options.parse(*args)
+        parsed_options[:options_help_text] = options.to_s
       end
       return parsed_options
     end
       
+    ##
+    # Load config file
+    #
+    # @param [String] config_file_name Configuration file name
+    # @return [Object] all the settings in the associated configuration file
+    #
+    def load_config_file(config_file_name)
+      contents = nil
+      begin
+        contents = YAML.load_file(config_file_name)
+      rescue Exception => ex
+        puts "SEVERE - Unable to load configuration file '#{config_file_name}'"
+        # puts ex.to_s
+        # puts ex.backtrace.join("\n")
+        contents = nil
+      end
+      return contents
+    end
   end
   
 end
--- spec/lib/ubsafe_config_spec.rb
+++ spec/lib/ubsafe_config_spec.rb
@@ -2,15 +2,44 @@ require File.expand_path(File.join(File.dirname(__FILE__), "..", "spec_helper"))
 
 describe 'UBSafe::Config' do
   
-  it 'should allow a configuration file to be specified from a environment parameter' 
-  
-  it 'should allow a configuration file to be specified from a parameter' do
-    config_file_name = 'ubsafe_config.yml'
-    args = ['-c',config_file_name]
-    config = UBSafe::Config.config(args)
-    puts config.options.inspect
+  before :each do
+    ENV['UBSAFE_CONFIG_FILE'] = nil
   end
   
+  it 'should allow a configuration file to be specified from an environment parameter'  do
+    config_file_name = './spec/test_data/ubsafe_config.yml'
+    ENV['UBSAFE_CONFIG_FILE'] = config_file_name
+    config = UBSafe::Config.config
+    config.load
+    config.options[:config_file].should == config_file_name
+  end
+  
+  it 'should allow a configuration file to be specified from a parameter' do
+    config_file_name = './spec/test_data/ubsafe_config.yml'
+    args = ['-c',config_file_name]
+    config = UBSafe::Config.config
+    config.load(args)
+    config.options[:config_file].should == config_file_name
+    config.options[:remainder].should == []
+  end
+  
+  it 'should override environment parameter with command-line parameter' do
+    config_file_name = 'ubsafe_config.yml'
+    ENV['UBSAFE_CONFIG_FILE'] = config_file_name
+    config_file_name_2 = './spec/test_data/ubsafe_config.yml'
+    args = ['-c',config_file_name_2]
+    config = UBSafe::Config.config
+    config.load(args)
+    config.options[:config_file].should == config_file_name_2
+  end
+  
+  it 'should load the configuration file and make the settings accessible' do
+    config_file_name = './spec/test_data/ubsafe_config.yml'
+    args = ['-c',config_file_name]
+    config = UBSafe::Config.config
+    config.load(args)
+    config.options[:test_settings][:setting_one].should == '1'
+  end
   
   
 end
